55a253e650764028cf19a08635dc7ad2f27984d5
marcos <marcosrodriguesbrugnaro@gmnail.com>
2019-10-17 16:34:42 -0300

    Initial configurations.

---
+++ src/main/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "styled-components";
+
+const View = styles.div`
+  background-color: #DDD;
+`;
+
+export default () => (
+  <View>
+    <p>test</p>
+  </View>
+);
